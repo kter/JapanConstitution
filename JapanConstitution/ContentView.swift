5cca67cbc376fd45c568d1704d59718a00c2d413
Tomohiko Takahashi <takahashi@tomohiko.io>
2024-06-15 01:11:45 +0900

    Initial Commit

---
+++ JapanConstitution/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JapanConstitution
+//
+//  Created by 高橋智彦 on 2024/06/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JapanConstitution/JapanConstitutionApp.swift
@@ -0,0 +1,17 @@
+//
+//  JapanConstitutionApp.swift
+//  JapanConstitution
+//
+//  Created by 高橋智彦 on 2024/06/15.
+//
+
+import SwiftUI
+
+@main
+struct JapanConstitutionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
